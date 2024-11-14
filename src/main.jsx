9ccff851977921116377d2d2cd10813fa01fcf19
Mangesh Namdeo Gosavi <148337560+Mangesh-Gosavi@users.noreply.github.com>
2024-11-14 11:24:40 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Importing BrowserRouter and Routes
-import Login from './Pages/Login';
-import Signup from './Pages/Signup';
-import Home from './Pages/Home';
-import Product from './Pages/Product';
-import Order from './Pages/Orders';
-import Cart from './Pages/Cart';
-import Forgot from './Pages/Forgot';
-import Newpass from './Pages/Newpass';
-import Search from './Pages/search';
-import Info from './Pages/Info';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from 'react-router-dom';
+import App from "../src/App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Info />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/Signup" element={<Signup />} />
-        <Route path="/Forgotpassword" element={<Forgot />} />
-        <Route path="/Newpassword" element={<Newpass />} />
-        <Route path="/Home" element={<Home />} />
-        <Route path="/Cart" element={<Cart />} />
-        <Route path="/Search" element={<Search />} />
-        <Route path="/Product/:id" element={<Product />} />
-        <Route path="/Order/:userid" element={<Order />} />
-      </Routes>
-    </Router>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
